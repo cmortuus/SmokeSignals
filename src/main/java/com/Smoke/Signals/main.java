9bab4108a0ed07b7b72974c70ade3b40a2e9ec64
redParrot17 <44844267+redparrot17@users.noreply.github.com>
2019-08-04 14:01:11 -0400

    Nightly upload

--- src/main/java/com/Smoke/Signals/main.java
+++ src/main/java/com/Smoke/Signals/main.java
@@ -7,6 +7,7 @@ public class main {
 
     public static void main(String[] args) throws IOException {
 
+        //User me = new User("Alex#123456");
         User me = new User("Christian#636174");
         me.initialize();
         connect(me);
@@ -15,8 +16,10 @@ public class main {
 
     private static void connect(User me) {
         String name;
-        try { name = me.joinRoom("Caleb#214628");
-        } catch (Exception e) { name = "Calebz214628Christianz636174"; }
+        try { //name = me.joinRoom("Christian#636174");
+            name = me.joinRoom("Caleb#214628");
+        } catch (Exception e) {
+            name = "Calebz214628Christianz636174"; }
         while (!me.isRoomReady(name))
             try { Thread.sleep(1000);
             } catch (InterruptedException ignore) {}
